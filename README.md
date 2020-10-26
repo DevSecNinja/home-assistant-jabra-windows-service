9b122288d5cbead1a0d68c1b5f0cb9a2be4d0736
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2020-10-26 14:33:21 +0100

    Align repository with Plantronics repo

--- README.md
+++ README.md
@@ -1,14 +1,44 @@
-## Create a secrets.ini in the root containing:
-````
-[DEFAULT]
-HomeAssistantURL = https://<HomeAssistantUrl>/api/states/sensor.jabraheadset
-HomeAssistantAuth = Bearer <Long Lived Access Token>
-````
+# Home Assistant Jabra Windows Service
 
-Tip: you can rename the `secrets.ini.example` file to `secrets.ini`.
+Welcome! This Python based Windows Service communicates the status of your Jabra headset to Home Assistant.
 
-## Run the Python script as service
+## Requirements
+- Python 3.* installed and added to PATH
+
+## How to install
+- Open a PowerShell window as administrator and run:
+
+```` powershell
+pip install -r .\requirements.txt
+````
+Make sure that the packages show up under `C:\Program Files (x86)\Python*\Lib\site-packages` as the Windows Service needs to access it.
+
+- Run the ![postinstall.py](https://github.com/mhammond/pywin32/blob/master/pywin32_postinstall.py) script from the PowerShell console with administrator privileges.
+
+- Clone or download the source code from this repository. Copy `secrets.example.ini` to `secrets.ini` and provide the required info.
+
+- Make sure the `SYSTEM` account has access to this folder as the service will run under `SYSTEM`.
+
+- Browse to the location and run the code below in PowerShell with administrator privileges:
+
+```` powershell
 python .\main.py --startup auto --wait 2 install
+````
 
-## If you want to debug the script
+## Troubleshooting
+- Check if the service is started. In PowerShell, run:
+```` powershell
+Get-Service -Name "JabraHomeAssistantSync"
+Get-Service -Name "JabraHomeAssistantSync" | Start-Service
+````
+
+- You can run the following code to debug the service:
+
+```` powershell
 python .\main.py --startup auto --wait 2 debug
+````
+
+- In case you receive the error `Error starting service: The service did not respond to the start or control request in a timely fashion` during the start of the service,
+make sure to have the `pywintypes36.dll` file available from the `C:\Program Files (x86)\Python*\Lib\site-packages\win32` folder. In case the file is missing,
+copy it from the `C:\Program Files (x86)\Python*\Lib\site-packages\pywin32_system32` folder.
+- Open up an issue within this repository in case your issue is not listed here or listed as a closed issue.
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import random
 import requests
 import json
@@ -6,14 +5,15 @@ import configparser
 import glob
 import sys
 import os
+import logging
 
 from pathlib import Path
-from SMWinservice import SMWinservice
-from requests import post
+from winservice import SMWinservice
 from time import sleep
 from msvcrt import kbhit
 
 import pywinusb.hid as hid
+
 
 class PythonCornerExample(SMWinservice):
     _svc_name_ = "JabraHomeAssistantSync"
@@ -37,43 +37,73 @@ class PythonCornerExample(SMWinservice):
             # get all currently connected HID devices we could filter by doing
             # something like hid.HidDeviceFilter(vendor_id = 0x1234), product Id
             # filters (with masks) and other capabilities also available
-            all_devices = hid.HidDeviceFilter(vendor_id = 0x0b0e).get_devices() # Vendor_id 0x0b0e = Jabra
+            all_devices = hid.HidDeviceFilter(
+                vendor_id=0x0B0E
+            ).get_devices()  # Vendor_id 0x0b0e = Jabra
 
             if not all_devices:
                 # add a delay of 0.5 seconds for resource utilization
                 sleep(0.5)
 
-                print("No HID class devices attached.")
+                logging.debug("No HID class devices attached.")
             else:
                 # search for our target usage (the hook button)
-                #target pageId, usageId
-                usage_telephony_hook = hid.get_full_usage_id(0xb, 0x20)
+                # target pageId, usageId
+                usage_telephony_hook = hid.get_full_usage_id(0xB, 0x20)
 
                 def hook_pressed(new_value, event_type):
                     "simple usage control handler"
                     # this simple handler is called on 'pressed' events
                     # this means the usage value has changed from '1' to '0'
                     # no need to check the value
-                    event_type = event_type #avoid pylint warnings
-                    
+                    event_type = event_type  # avoid pylint warnings
+
                     # import the config file
                     config = configparser.ConfigParser()
                     config.read(os.path.join(sys.path[0], "secrets.ini"))
 
                     if new_value:
-                        print("On Hook!")
+                        logging.info("On Hook!")
                         response = requests.post(
-                                config.get('DEFAULT', 'HomeAssistantURL'),
-                                headers={'Authorization': config.get('DEFAULT', 'HomeAssistantAuth'), 'content-type': 'application/json'},
-                                data=json.dumps({'state': 'on', 'attributes': {'friendly_name': 'Jabra Headset','icon': 'mdi:headset'}}))
-                        print(response.text)
+                            config.get("DEFAULT", "HomeAssistantURL"),
+                            headers={
+                                "Authorization": config.get(
+                                    "DEFAULT", "HomeAssistantAuth"
+                                ),
+                                "content-type": "application/json",
+                            },
+                            data=json.dumps(
+                                {
+                                    "state": "on",
+                                    "attributes": {
+                                        "friendly_name": "Jabra Headset",
+                                        "icon": "mdi:headset",
+                                    },
+                                }
+                            ),
+                        )
+                        logging.info(response.text)
                     else:
-                        print("Off Hook!")
+                        logging.info("Off Hook!")
                         response = requests.post(
-                                config.get('DEFAULT', 'HomeAssistantURL'),
-                                headers={'Authorization': config.get('DEFAULT', 'HomeAssistantAuth'), 'content-type': 'application/json'},
-                                data=json.dumps({'state': 'off', 'attributes': {'friendly_name': 'Jabra Headset','icon': 'mdi:headset'}}))
-                        print(response.text)
+                            config.get("DEFAULT", "HomeAssistantURL"),
+                            headers={
+                                "Authorization": config.get(
+                                    "DEFAULT", "HomeAssistantAuth"
+                                ),
+                                "content-type": "application/json",
+                            },
+                            data=json.dumps(
+                                {
+                                    "state": "off",
+                                    "attributes": {
+                                        "friendly_name": "Jabra Headset",
+                                        "icon": "mdi:headset",
+                                    },
+                                }
+                            ),
+                        )
+                        logging.info(response.text)
 
                 for device in all_devices:
                     try:
@@ -84,28 +114,40 @@ class PythonCornerExample(SMWinservice):
 
                         for input_report in all_input_reports:
                             if usage_telephony_hook in input_report:
-                                #found a telephony device w/ hook button
-                                print("\nMonitoring {0.vendor_name} {0.product_name} "\
-                                        "device.\n".format(device))
+                                # found a telephony device w/ hook button
+                                logging.info(
+                                    "\nMonitoring {0.vendor_name} {0.product_name} "
+                                    "device.\n".format(device)
+                                )
 
                                 # add event handler (example of other available
                                 # events: EVT_PRESSED, EVT_RELEASED, EVT_ALL, ...)
-                                device.add_event_handler(usage_telephony_hook,
-                                    hook_pressed, hid.HID_EVT_CHANGED) #level usage
+                                device.add_event_handler(
+                                    usage_telephony_hook,
+                                    hook_pressed,
+                                    hid.HID_EVT_CHANGED,
+                                )  # level usage
 
                                 if input_interrupt_transfers:
                                     # poll the current value (GET_REPORT directive),
                                     # allow handler to process result
                                     input_report.get()
 
-                                while not kbhit() and device.is_plugged() and self.isrunning:
-                                    #just keep the device opened to receive events
+                                while (
+                                    not kbhit()
+                                    and device.is_plugged()
+                                    and self.isrunning
+                                ):
+                                    # just keep the device opened to receive events
                                     sleep(0.5)
                                 # return - Uncomment if you want the service to stop
                     finally:
                         device.close()
-                print("Sorry, no one of the attached HID class devices "\
-                    "provide any Telephony Hook button")
+                logging.info(
+                    "Sorry, no one of the attached HID class devices "
+                    "provide any Telephony Hook button"
+                )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     PythonCornerExample.parse_command_line()
